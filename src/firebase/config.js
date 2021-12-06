e6106ab2dd7242ad926e8e7ac201627e80d9d2d0
Nikhil Rajput <nkr.nikhil.nkr@gmail.com>
2021-12-06 12:11:37 +0530

    update changes and bug fixes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,3 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB4junA-A_4qZFxb_nPx7B85cfweYX8Bi0",
-    authDomain: "gyan-sangrah.firebaseapp.com",
-    projectId: "gyan-sangrah",
-    storageBucket: "gyan-sangrah.appspot.com",
-    messagingSenderId: "481784142175",
-    appId: "1:481784142175:web:aade7df752efe935ca7eea",
-    measurementId: "G-ZC0K5T0K8Z"
+    FIREBASE_CONFIG
 };
